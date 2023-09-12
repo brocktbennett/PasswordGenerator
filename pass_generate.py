e66f4e41271bc4cebd27091e92e37ba65929126a
Brock T. Bennett <brock.bennett@okstate.edu>
2023-09-12 17:56:52 -0500

    Enhance password generator with user-customizable options and output to text file. Added some functionality to show people how to code your own personal password generator.

--- pass_generate.py
+++ pass_generate.py
@@ -1,14 +1,12 @@
 import random  # Import the random library for generating random characters
 import time  # Import the time library for sleep function
 
-
 # Function to simulate typing like a human for fun effect
 def slow_print(text, delay=0.03):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
     print()  # Print newline at the end
-
 
 # Function to generate a password based on the user's requirements
 def generate_password(length, use_lower, use_upper, use_numbers):
@@ -31,21 +29,23 @@ def generate_password(length, use_lower, use_upper, use_numbers):
     # Generate and return the password
     return "".join(random.sample(all_characters, length))
 
-
 # Main code starts here
 if __name__ == "__main__":
-    slow_print("🎉🎉🎉 Welcome to the MOST FUN Dynamic Password Generator EVER! 🎉🎉🎉")
+    slow_print("Welcome to the most fun Dynamic Password Generator ever!")
+
+    # Get the username
+    username = input("First, what's your username? ")
 
     # Get password length from the user
     while True:
         try:
-            length = int(input("How LONG should your password be? (e.g. 12): "))
+            length = int(input("How long should your password be? (e.g. 12): "))
             if length < 1:
-                slow_print("A password should have at least 1 character, c'mon! 🙃")
+                slow_print("A password should have at least 1 character, come on!")
             else:
                 break
         except ValueError:
-            slow_print("That's not a number! Try again, genius! 🤓")
+            slow_print("That's not a number! Try again.")
 
     # Get user's character preferences
     use_lower = input("Want some lower-case characters? (y/n): ").strip().lower() == 'y'
@@ -53,18 +53,19 @@ if __name__ == "__main__":
     use_numbers = input("You like numbers, don't you? (y/n): ").strip().lower() == 'y'
 
     # Ask the user what the password is for
-    purpose = input("What's this password for? Make it juicy! 🍉: ")
+    purpose = input("What's this password for? ")
 
     # Generate the password
     password = generate_password(length, use_lower, use_upper, use_numbers)
 
     if password:
-        slow_print(f"🌟 Your super-secure, absolutely amazing password is: {password} 🌟")
+        slow_print("Your super-secure, absolutely amazing password is: " + password)
 
         # Save the password to a text file
         with open("passwords.txt", "a") as f:
-            f.write(f"Juicy Details: {purpose}\n")
-            f.write(f"Super-Secret Password: {password}\n")
+            f.write(f"Username: {username}\n")
+            f.write(f"Details: {purpose}\n")
+            f.write(f"Password: {password}\n")
             f.write("=" * 30 + "\n")
 
-        slow_print("📝 Your password is now saved in a 'passwords.txt' file, as safe as a treasure chest! 🏴‍☠️")
+        slow_print("Your password is now saved in a 'passwords.txt' file.")
