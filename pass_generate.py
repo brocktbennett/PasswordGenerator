3d1b00f91345bbeb6fdd94247832e67dbdfd998e
Brock T. Bennett <brock.bennett@okstate.edu>
2023-09-12 17:53:10 -0500

    Enhance password generator with user-customizable options and output to text file. Added some functionality to show people how to code your own personal password generator.

--- pass_generate.py
+++ pass_generate.py
@@ -1,50 +1,70 @@
-import random
+import random  # Import the random library for generating random characters
+import time  # Import the time library for sleep function
 
 
-def generate_password(length, use_lower, use_upper, use_numbers, use_symbols):
+# Function to simulate typing like a human for fun effect
+def slow_print(text, delay=0.03):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()  # Print newline at the end
+
+
+# Function to generate a password based on the user's requirements
+def generate_password(length, use_lower, use_upper, use_numbers):
+    # Adding a dramatic pause for effect
+    slow_print("Generating your super-secure password...", 0.05)
+
+    # Define available characters based on the user's choice
     lower_case = "abcdefghijklmnopqrstuvwxyz" if use_lower else ""
     upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" if use_upper else ""
     numbers = "0123456789" if use_numbers else ""
-    symbols = "@#$%^&*/\?" if use_symbols else ""
 
-    all_characters = lower_case + upper_case + numbers + symbols
+    # Combine all selected characters into a single string
+    all_characters = lower_case + upper_case + numbers
 
+    # Check if at least one type of character is selected
     if len(all_characters) == 0:
-        print("You need to select at least one type of character to generate a password!")
+        slow_print("Oops! You need to select at least one type of character.")
         return None
 
+    # Generate and return the password
     return "".join(random.sample(all_characters, length))
 
 
+# Main code starts here
 if __name__ == "__main__":
-    print("Welcome to the Dynamic Password Generator! 🎉")
+    slow_print("🎉🎉🎉 Welcome to the MOST FUN Dynamic Password Generator EVER! 🎉🎉🎉")
 
-    # Get user preferences
-    try:
-        length = int(input("Enter the length for your password (e.g. 12): "))
-        if length < 1:
-            print("Password length should be at least 1.")
-            exit()
-    except ValueError:
-        print("Invalid input! Please enter a number.")
-        exit()
+    # Get password length from the user
+    while True:
+        try:
+            length = int(input("How LONG should your password be? (e.g. 12): "))
+            if length < 1:
+                slow_print("A password should have at least 1 character, c'mon! 🙃")
+            else:
+                break
+        except ValueError:
+            slow_print("That's not a number! Try again, genius! 🤓")
 
-    use_lower = input("Include lower-case characters? (y/n): ").strip().lower() == 'y'
-    use_upper = input("Include upper-case characters? (y/n): ").strip().lower() == 'y'
-    use_numbers = input("Include numbers? (y/n): ").strip().lower() == 'y'
-    use_symbols = input("Include symbols? (y/n): ").strip().lower() == 'y'
+    # Get user's character preferences
+    use_lower = input("Want some lower-case characters? (y/n): ").strip().lower() == 'y'
+    use_upper = input("How about upper-case characters? (y/n): ").strip().lower() == 'y'
+    use_numbers = input("You like numbers, don't you? (y/n): ").strip().lower() == 'y'
 
-    purpose = input("What will you use this password for? ")
+    # Ask the user what the password is for
+    purpose = input("What's this password for? Make it juicy! 🍉: ")
 
-    password = generate_password(length, use_lower, use_upper, use_numbers, use_symbols)
+    # Generate the password
+    password = generate_password(length, use_lower, use_upper, use_numbers)
 
     if password:
-        print(f"Your super-secure password is: {password} 🌟")
+        slow_print(f"🌟 Your super-secure, absolutely amazing password is: {password} 🌟")
 
-        # Save to txt file
+        # Save the password to a text file
         with open("passwords.txt", "a") as f:
-            f.write(f"Purpose: {purpose}\n")
-            f.write(f"Password: {password}\n")
+            f.write(f"Juicy Details: {purpose}\n")
+            f.write(f"Super-Secret Password: {password}\n")
             f.write("=" * 30 + "\n")
 
-        print("Your password has been saved to 'passwords.txt'. 📝")
+        slow_print("📝 Your password is now saved in a 'passwords.txt' file, as safe as a treasure chest! 🏴‍☠️")
